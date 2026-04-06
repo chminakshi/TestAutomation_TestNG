d12c564ac9ed2e76fcc25de9c896a5b67e1cac17
chminakshi <ch_minakshi@yahoo.com>
2026-04-06 19:54:28 +0530

    First commit

---
+++ src/main/java/allPracticeRahulshetty/TestAutomation_TestNg/App.java
@@ -0,0 +1,7 @@
+package allPracticeRahulshetty.TestAutomation_TestNg;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/TestNGPracticePKG/TestNGPracticeClass.java
@@ -0,0 +1,76 @@
+package TestNGPracticePKG;
+import java.sql.Driver;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestNGPracticeClass {
+	
+	
+	
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	
+	
+	@BeforeMethod
+	@Parameters("browser")
+	
+	public void launchApplication(String browser)
+	{
+		if(browser.equalsIgnoreCase("chrome"))
+		{
+	    driver.set(new  ChromeDriver());
+		driver.get();
+		driver.get().get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get().manage().window().maximize();
+		
+		}else if (browser.equalsIgnoreCase("firefox")) {
+			
+			 driver.set(new EdgeDriver());
+			driver.get();
+			driver.get().get("https://rahulshettyacademy.com/AutomationPractice/");
+			driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get().manage().window().maximize();
+				
+			
+		}
+		
+	}
+	
+	
+	@Test
+	public void radiobutton() throws InterruptedException {
+
+		driver.get().findElement(By.cssSelector("[value='radio1']")).click();
+		driver.get().findElement(By.cssSelector("[value='radio2']")).click();
+		driver.get().findElement(By.cssSelector("[value='radio3']")).click();
+
+	}
+
+	@Test
+	public void selectdropdown() {
+
+		driver.get().findElement(By.cssSelector("input#autocomplete")).click();
+		driver.get().findElement(By.cssSelector("input#autocomplete")).sendKeys("Ind");
+		driver.get().findElement(By.xpath("//div[text()='India']")).click();
+
+	}
+	
+	
+	
+	@AfterMethod
+	
+	public void tearDown()
+	{
+	  driver.get().quit();
+	}
+
+}
