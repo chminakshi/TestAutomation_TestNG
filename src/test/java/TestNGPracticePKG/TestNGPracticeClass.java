31a14ba6dc3d3698568b2f8b19b16d17543cb302
chminakshi <ch_minakshi@yahoo.com>
2026-04-08 15:11:28 +0530

    test

--- src/test/java/TestNGPracticePKG/TestNGPracticeClass.java
+++ src/test/java/TestNGPracticePKG/TestNGPracticeClass.java
@@ -52,7 +52,7 @@ public class TestNGPracticeClass {
 		driver.get().findElement(By.cssSelector("[value='radio1']")).click();
 		driver.get().findElement(By.cssSelector("[value='radio2']")).click();
 		driver.get().findElement(By.cssSelector("[value='radio3']")).click();
-		System.out.println("Hello Minakshi1");
+		System.out.println("Hello Minakshi12");
 
 	}
 
